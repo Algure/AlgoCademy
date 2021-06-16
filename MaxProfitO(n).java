8d094165f3f6917c363e2ef5c4fd50160fc6fe84
Ajiri Gunn <37802577+Algure@users.noreply.github.com>
2021-06-16 23:10:15 +0100

    Solution to Best Time to Buy and Sell Stock III - O(n)
    Best Time to Buy and Sell Stock III - O(n)
    
    Say you have an array for which the i-th element is the price of a given stock on day i.
    
    Design an algorithm to find the maximum profit. You may complete at most k transactions.
    
    Note:
    You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
    
    Example 1:
    
    Input: [2,4,1], k = 2
    Output: 2
    Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
    Example 2:
    
    Input: [3,2,6,5,0,3], k = 2
    Output: 7
    Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
                 Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.

---
+++ MaxProfitO(n).java
@@ -0,0 +1,57 @@
+class Solution {
+
+    List<Integer> computeProfits(List<Integer[]> bottomTopPrices) {
+        Stack<Integer[]> stack = new Stack<>();
+        List<Integer> profits = new ArrayList<>();
+        for (Integer[] bottomTop : bottomTopPrices) {
+            int bottom = bottomTop[0];
+            int top = bottomTop[1];
+            while (!stack.empty() && bottom < stack.peek()[0]) {
+                profits.add(stack.peek()[1] - stack.peek()[0]);
+                stack.pop();
+            }
+            while (!stack.empty() && top >= stack.peek()[1]) {
+                profits.add(stack.peek()[1] - bottom);
+                bottom = stack.peek()[0];
+                stack.pop();
+            }
+            stack.add(new Integer[]{bottom, top});
+        }
+        while (!stack.empty()) {
+            profits.add(stack.peek()[1] - stack.peek()[0]);
+            stack.pop();
+        }
+        return profits;
+    }
+
+    List<Integer[]> findBottomTopPrices(int[] prices) {
+        int n = prices.length, i = 0;
+        List<Integer[]> bottomTopPrices = new ArrayList<>();
+        while (i < n) {
+            int bottom = i;
+            while (bottom + 1 < n && prices[bottom] >= prices[bottom + 1]) {
+                ++bottom;
+            }
+            int top = bottom;
+            while (top + 1 < n && prices[top] <= prices[top + 1]) {
+                ++top;
+            }
+            bottomTopPrices.add(new Integer[]{prices[bottom], prices[top]});
+            i = top + 1;
+        }
+        return bottomTopPrices;
+    }
+
+    int maxProfit(int[] prices, int k) {
+        List<Integer[]> bottomTopPrices = findBottomTopPrices(prices);
+        List<Integer> profits = computeProfits(bottomTopPrices);
+        k = Math.min(k, profits.size());
+        Collections.sort(profits, (a, b) -> b - a);
+        int result = 0;
+        for (int i = 0; i < k; ++i) {
+            result += profits.get(i);
+        }
+        return result;
+    }
+};
+
